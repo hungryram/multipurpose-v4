18c7c05a896f4f316b1bbd1c762dd188118d437d
Ram Dettmer <ram@hungryram.com>
2025-05-01 15:45:37 -0700

    upate revalidation

--- web/lib/sanity.ts
+++ web/lib/sanity.ts
@@ -6,5 +6,5 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: '2023-05-07',
-  useCdn: true, // or false if you want fresh data every time
+  useCdn: false, // or false if you want fresh data every time
 })
